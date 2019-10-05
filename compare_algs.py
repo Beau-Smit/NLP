9cd66134fb3b8843023ad1d53cf8a3ff9ab45140
Beau Smit <beau.h.smit@gmail.com>
2019-10-05 16:33:10 -0400

    compare time between packages

---
+++ compare_algs.py
@@ -0,0 +1,44 @@
+# package imports
+import pandas as pd
+import numpy as np
+import jellyfish
+import textdistance
+# import Levenshtein
+
+# python built-ins
+from itertools import groupby
+import time
+
+# CONCLUSION: jellyfish is much faster than textdistance
+
+# print(jellyfish.soundex('Beau'))
+# print(jellyfish.soundex('Smit'))
+# print(jellyfish.levenshtein_distance('Beau', 'Smit'))
+# print(jellyfish.levenshtein_distance('hi', 'hello'))
+
+
+myseries = pd.Series(['plumbob', 'plumbus', 'plumbing'] * 50000)
+
+@profile
+def jelly_lev(str1, str2):
+   distance = jellyfish.levenshtein_distance(str1, str2)
+   return distance
+
+myseries.apply(jelly_lev, str2='plumber')
+
+@profile
+def textd_lev(str1, str2):
+   distance = textdistance.levenshtein.distance(str1, str2)
+   return distance
+
+myseries.apply(textd_lev, str2='plumber')
+
+# @profile
+# def Leven_lev(str1, str2):
+#    distance = Levenshtein.distance(str1, str2)
+#    return distance
+
+# myseries.apply(Leven_lev, str2='plumber')
+
+
+
---
+++ time_profile.sh
@@ -0,0 +1,3 @@
+kernprof -l compare_algs.py
+
+python -m line_profiler compare_algs.py.lprof | tee compare_algs_profiling.txt
